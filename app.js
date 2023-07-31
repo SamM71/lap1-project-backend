f6625471062088dd40f3f43dc5849deb2e800ed8
smithnj13 <SmithNJ13@hotmail.com>
2023-07-31 09:31:35 +0100

    added logger and /capitals get

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
+const logger = require("./logger");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 app.get("/", (req, res) => {
     res.status(200).send("root page working!");
 })
+
+app.get("/capitals", (req, res) => {
+    res.status(200).send("")
+})
+
+
 module.exports = app;
