34f86dfc736ba8757f35be3a022d6b88670eda74
smithnj13 <SmithNJ13@hotmail.com>
2023-07-31 11:05:12 +0100

    added post functionality

--- app.js
+++ app.js
@@ -9,12 +9,22 @@ app.use(cors());
 app.use(express.json());
 app.use(logger);
 
+function random() {
+    const num = Math.floor(Math.random() * countries.length);
+    return num;
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("welcome to our app");
 })
 
 app.get("/countries", (req, res) => {
     res.status(200).send(countries);
+})
+
+app.get("/countries/random", (req, res) => {
+    const country = countries[random()];
+    res.status(200).send(country);
 })
 
 app.get("/countries/:id", (req, res) => {
@@ -27,8 +37,11 @@ app.get("/countries/:id", (req, res) => {
     }
 })
 
+// Potentially useful code ?
 app.post("/countries", (req, res) => {
-    res.status(201).send("submission posted!");
+    const newCountry = req.body;
+    countries.push(newCountry); 
+    res.status(201).send(newCountry);
 })
 
 module.exports = app;
